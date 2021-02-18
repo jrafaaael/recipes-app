adb16543c933a78b7b91ca3d7346cd38ec1e9a34
jrafaaael <jrmd070102@gmail.com>
2021-02-18 16:14:40 -0400

    added a class for new meals

--- JS/set_meal_information.js
+++ JS/set_meal_information.js
@@ -1,34 +1,51 @@
+// Class
+class Meal {
+    constructor(
+        name, area, category, instructions,
+        img, youtubeVideoKey, ingredients) {
+        this.name = name;
+        this.area = area;
+        this.category = category;
+        this.instructions = instructions;
+        this.img = img;
+        this.youtubeVideoKey = youtubeVideoKey;
+        this.ingredients = ingredients;
+        this.instructionsLong = (
+            (this.instructions.length > 800) &&
+            !(this.instructions.length < 850))
+    }
+}
+
 // Exports
 export const setMealInformation = (mealData) => {
     const ingredients =
-    Object.entries(mealData)
-        .filter(allDataIngredient => {
-            return allDataIngredient[0].includes('Ingredient')
-        })
-        .filter(notIngredientsEmpty => {
-            return (
-                notIngredientsEmpty[1] !== '' &&
-                notIngredientsEmpty[1] !== ' ' &&
-                notIngredientsEmpty[1] !== null);
-        })
-        .map((ingredient, index) => {
-            return [
-                ingredient[1],
-                ((mealData[`strMeasure${index + 1}`] === '') ||
-                (mealData[`strMeasure${index + 1}`] === ' ') ||
-                (mealData[`strMeasure${index + 1}`] === null)) ? 
-                'to taste' : (mealData[`strMeasure${index + 1}`])
-            ]
-            .join(' — ');
-        });
+        Object.entries(mealData)
+            .filter(allDataIngredient => {
+                return allDataIngredient[0].includes('Ingredient')
+            })
+            .filter(notIngredientsEmpty => {
+                return (
+                    notIngredientsEmpty[1] !== '' &&
+                    notIngredientsEmpty[1] !== ' ' &&
+                    notIngredientsEmpty[1] !== null);
+            })
+            .map((ingredient, index) => {
+                return [
+                    ingredient[1],
+                    ((mealData[`strMeasure${index + 1}`] === '') ||
+                        (mealData[`strMeasure${index + 1}`] === ' ') ||
+                        (mealData[`strMeasure${index + 1}`] === null)) ?
+                        'to taste' : (mealData[`strMeasure${index + 1}`])
+                ]
+                    .join(' — ');
+            });
 
-    return {
-        name: mealData.strMeal,
-        area: mealData.strArea,
-        category: mealData.strCategory,
-        instructions: mealData.strInstructions,
-        img: mealData.strMealThumb,
-        youtubeVideoKey: mealData.strYoutube.slice(-11),
-        ingredients: ingredients,
-    }
+    return new Meal(
+        mealData.strMeal,
+        mealData.strArea,
+        mealData.strCategory,
+        mealData.strInstructions,
+        mealData.strMealThumb,
+        mealData.strYoutube.slice(-11),
+        ingredients);
 }
